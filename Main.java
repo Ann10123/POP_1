ec2b1883b8fc6bb656977cb084bf0ae1021871de
Ann10123 <152207473+Ann10123@users.noreply.github.com>
2026-03-05 01:40:42 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -47,7 +47,7 @@ public class Main {
         
         for (CalculatorTask task : tasks) {
             try {
-                Thread.sleep(500); 
+                Thread.sleep(1000); 
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
